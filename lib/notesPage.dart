826b7eafc56bd695c1ed1e54132c55c02f637c6f
Randy Ankiambom <jamrandyankiambom@gmail.com>
2023-08-28 09:19:54 +0100

    changed the ui

--- lib/notesPage.dart
+++ lib/notesPage.dart
@@ -9,113 +9,82 @@ class NotesPage extends StatefulWidget {
 
 class _NotesPageState extends State<NotesPage> {
 
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+  int myIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          color: Colors.black,
-        ),
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.fromLTRB(8, 70, 8, 8),
-              child: Row(
+      backgroundColor: Colors.black,
+      body: Column(
+          children:<Widget>[
+            Container(
+              margin: const EdgeInsets.all(20.0),
+              padding: const EdgeInsets.all(12.0),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Text(
-                    'Notes',
-                    style: TextStyle(
-                      fontSize: 40,
-                      color: Colors.white,
-                    ),
-                  ),
+                  Text("Notes",style: TextStyle(color:Colors.white,fontSize:20),),
                 ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Container(
-                 decoration: BoxDecoration(
-                  color: Colors.grey,
-                   shape: BoxShape.rectangle,
-                   borderRadius: BorderRadius.circular(30),
-                ),
-                 child: TextField(
-                  decoration: InputDecoration(
-                      hintText: 'Search notes',
-                      prefixIcon: const Icon(Icons.search),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      )
-                  ),
-                ),
+            Container(
+              margin: const EdgeInsets.all(20.0),
+              padding: const EdgeInsets.fromLTRB(8, 3, 3, 3),
+              decoration:BoxDecoration(
+                  borderRadius:BorderRadius.circular(50),
+                  color:Colors.brown,
               ),
+              child: const TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  icon: Icon(Icons.search),
+                  hintText: 'Search Notes',
+
+                ),
+              )
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: Colors.grey,
-                ),
-                child: SingleChildScrollView(
-                  child: ListView.builder(
-                    itemCount: 3,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
-                        title: Text('Item $index'),
-                      );
-                    },
-                  ),
-                ),
+            Container(
+              margin: const EdgeInsets.all(20.0),
+              padding: const EdgeInsets.all(12.0),
+              decoration:BoxDecoration(
+                  borderRadius:BorderRadius.circular(8),
+                  color:Colors.grey,
               ),
+              child: const Text('Task'),
             ),
           ],
-        ),
       ),
-      floatingActionButton: const Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FloatingActionButton(
-            onPressed: null,
-            backgroundColor: Colors.blueAccent,
-            foregroundColor: Colors.white,
-            hoverColor: Colors.grey,
-            elevation: 12,
-            mouseCursor: MaterialStateMouseCursor.clickable,
-            child: Icon(Icons.add),
-          ),
-        ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        child: const Icon(Icons.add),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.black87,
-        iconSize: 30,
-        mouseCursor: SystemMouseCursors.grab,
-        selectedIconTheme: const IconThemeData(color: Colors.blueAccent),
-        selectedItemColor: Colors.blueAccent,
-        unselectedItemColor: Colors.white,
-        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-
-        items: const <BottomNavigationBarItem> [
+        backgroundColor: Colors.brown,
+        onTap: (index) {
+          setState(() {
+            myIndex = index;
+          });
+        },
+        currentIndex: myIndex,
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.book),
             label: 'Notes',
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.done),
-              label: 'To-dos'
+            icon: Icon(Icons.done),
+            label: 'To-dos',
           )
         ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
+        iconSize: 30,
+        fixedColor: Colors.blue,
       ),
     );
   }
 }
+
+
+
